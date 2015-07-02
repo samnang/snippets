34c6656de61597d711b6d0a1df48ab994b6de222
Samnang Chhun <samnang.chhun@gmail.com>
2015-07-02 15:03:44 +0700

    Refactor snippet feature spec

--- spec/features/user_creates_a_snippet_spec.rb
+++ spec/features/user_creates_a_snippet_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 feature 'User creates a snippet' do
   scenario 'successfully created' do
-    visit root_path
-    click_on 'Create Snippet'
-
-    fill_in :snippet_content, with: 'My snippet'
-    click_on 'Save'
+    create_snippet('My snippet')
 
     expect(page).to have_content('My snippet')
   end
 
   scenario 'falied to create' do
+    create_snippet('')
+
+    expect(page).to have_content("Content can't be blank")
+  end
+
+  def create_snippet(content)
     visit root_path
     click_on 'Create Snippet'
 
-    fill_in :snippet_content, with: ''
+    fill_in :snippet_content, with: content
     click_on 'Save'
-
-    expect(page).to have_content("Content can't be blank")
   end
 end
